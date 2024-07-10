a0319434557de4968e5dc047e1e7cd5cd3069cc5
Dev-socar <omar.fsc19@gmail.com>
2024-07-09 23:25:47 -0600

    Config CORS

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ dotenv.config();
 conectarDB();
 const dominiosPermitidos = [process.env.FRONTEND_URL];
 const corsOptions = {
- origin: function (origin, callback) {
-  if (dominiosPermitidos.indexOf(origin) !== -1) {
-   //El origin del request esta permitido
-   callback(null, true);
-  } else {
-      callback(new Error('No permitido por CORS'));
-  }
- },
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      //El origin del request esta permitido
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
 };
 
-app.use(cors(corsOptions))
+app.use(cors({ origin: "*" }));
 
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
- console.log(`Servidor funcionando en el Puerto ${PORT}`);
+  console.log(`Servidor funcionando en el Puerto ${PORT}`);
 });
